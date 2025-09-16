36232360632d718782ee4988c501a3fb08dc9e52
Shamsuzzaha Sumon <shamsuzzahasumon@gmail.com>
2025-09-16 17:45:58 +0600

    use of store

--- Youtube Practice/Anisul Islam Channel/Redux  Redux-toolkit Bangla Tutorials/11 react-redux/src/components/todos.jsx
+++ Youtube Practice/Anisul Islam Channel/Redux  Redux-toolkit Bangla Tutorials/11 react-redux/src/components/todos.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import {useSelector} from "react-redux";
 
 const Todos = () => {
 
+    const {isLoading, todos, error} = useSelector((state) => state);
+
+
     return (
         <div>
-            <h1>Todos</h1>
+            <h2>Todos App</h2>
+            {isLoading ? ('hello'):"false"}
         </div>
     );
 };
